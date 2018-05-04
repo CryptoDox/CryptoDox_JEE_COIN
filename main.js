93e7f5cdb0a7b434e7bed969fad7134d431cd0f7
CryptoDox <paradoxal@sfr.fr>
2018-05-04 22:37:24 +0200

    main.js Second run / add intégrité verif. (debug)

--- main.js
+++ main.js
@@ -32,10 +32,34 @@ class Blockchain{
         nouveauBlock.hash = nouveauBlock.blockHash();
         this.chain.push(nouveauBlock);
     }
+
+    inviolableChain(){
+        for(let i = 1; i < this.chain.length; i++){
+            const ceBlock = this.chain[i];
+            const precedantBlock = this.chain[i - 1];
+
+            if(ceBlock.hash !== ceBlock.blockHash()){
+                return false;
+            }
+
+            if(ceBlock.precedantHash !== precedantBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let CryptoDox_JEE_COIN = new Blockchain();
 CryptoDox_JEE_COIN.ajouterBlock(new Block(1, "02/05/2018", { montant: 5}));
 CryptoDox_JEE_COIN.ajouterBlock(new Block(2, "03/05/2018", { montant: 12}));
 
-console.log(JSON.stringify(CryptoDox_JEE_COIN, null, 4));
+console.log('Intégrité Blockchain ? ' + CryptoDox_JEE_COIN.inviolableChain());
+
+CryptoDox_JEE_COIN.chain[1].infos = { montant: 100};
+CryptoDox_JEE_COIN.chain[1].hash = CryptoDox_JEE_COIN.chain[1].blockHash();
+
+console.log('Intégrité Blockchain ? ' + CryptoDox_JEE_COIN.inviolableChain());
+
+//console.log(JSON.stringify(CryptoDox_JEE_COIN, null, 4));
